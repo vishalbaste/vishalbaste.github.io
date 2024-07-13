48df5af869a10a2c5caaf7e29748e23ab139921c
vishalbaste <vishalbaste726@gmail.com>
2024-07-14 00:32:34 +0530

    Back To Top Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,4 +22,22 @@ function writeTxt(element,speed,repet=false)
     }
 }
 
-writeTxt(document.querySelector('#Home h2'),50)
+writeTxt(document.querySelector('#Home h2'),50);
+
+
+// Scroll to Top
+let bagToTop = document.getElementById('back-to-top');
+document.onscroll = () => BackTop();
+function BackTop()
+{
+    let bagToTopValue = document.getElementById('progress');
+    let scrTop = document.documentElement.scrollTop;
+    let scrollVal = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round(scrTop * 100 / scrollVal);
+    if (document.documentElement.scrollTop > 20) {
+        bagToTop.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+        bagToTop.style.display = "flex";
+    } else bagToTop.style.display = "none";
+}
+
+bagToTop.addEventListener("click", () => document.documentElement.scrollTop = 0);
